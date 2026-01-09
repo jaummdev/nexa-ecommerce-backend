64ceb14e714b08180832c68f09a2b01964e78a8b
João Carlos <joaocarlosbdesena@gmail.com>
2026-01-09 00:27:14 -0300

    feat: implement banner management functionality
    - Added update and delete methods to BannersController for managing banners
    - Enhanced responses with success messages for create, update, and delete operations
    - Updated routes to include PUT and DELETE endpoints for banner management

--- src/controllers/banners/banners.controller.ts
+++ src/controllers/banners/banners.controller.ts
@@ -17,9 +17,55 @@ export class BannersController {
       const banner = await prisma.banner.create({
         data: { title, image_url },
       });
-      return res.status(200).json({ banner });
+      return res
+        .status(200)
+        .json({ message: "Banner created successfully", banner });
     } catch (error) {
-      return res.status(500).json({ message: "Internal server error" });
+      return res
+        .status(500)
+        .json({ message: "Internal server error", error: error });
+    }
+  }
+
+  static async updateBanner(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { title, image_url } = req.body;
+      const banner = await prisma.banner.update({
+        where: { id: Number(id) },
+        data: { title, image_url },
+      });
+      return res
+        .status(200)
+        .json({ message: "Banner updated successfully", banner });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Internal server error", error: error });
+    }
+  }
+
+  static async deleteBanner(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      const banner = await prisma.banner.findUnique({
+        where: { id: Number(id) },
+      });
+
+      if (!banner) {
+        return res.status(404).json({ message: "Banner not found to delete" });
+      }
+
+      await prisma.banner.delete({
+        where: { id: Number(id) },
+      });
+
+      return res.status(200).json({ message: "Banner deleted successfully" });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Internal server error", error: error });
     }
   }
 }
--- src/routes/banners/banners.routes.ts
+++ src/routes/banners/banners.routes.ts
@@ -7,5 +7,7 @@ const router: Router = Router();
 
 router.get("/", auth(Role.ADMIN), BannersController.getBanners);
 router.post("/", auth(Role.ADMIN), BannersController.createBanner);
+router.put("/:id", auth(Role.ADMIN), BannersController.updateBanner);
+router.delete("/:id", auth(Role.ADMIN), BannersController.deleteBanner);
 
 export default router;
